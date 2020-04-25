bcf0ccb23d8d1206bbb6d245824730b63a8aa4ce
Anastasia <moroznastia20202@mail.ru>
2020-04-25 12:15:24 +0300

    Verstka

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule,
